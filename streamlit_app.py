c71b4a0ca5a61981846cb769d254b4205ec2ea73
mehdi-elion <mehdi.elion@gmail.com>
2021-09-26 04:21:54 +0200

    [ENH] cleaned code with pre-commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
-import joblib
 import re
-import numpy as np
-from sklearn.neural_network import MLPClassifier
-from sklearn.feature_extraction.text import TfidfVectorizer
-import streamlit as st
 
-from lime.lime_text import LimeTextExplainer
+import joblib
+import numpy as np
+import streamlit as st
 import streamlit.components.v1 as components
+from lime.lime_text import LimeTextExplainer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neural_network import MLPClassifier
 
 
 # create a header
@@ -18,12 +18,13 @@ message_text = st.text_input("Enter a message for spam evaluation")
 
 # loading the model
 def preprocessor(text):
-    text = re.sub('<[^>]*>', '', text) 
-    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
-    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
+    text = re.sub("<[^>]*>", "", text)
+    emoticons = re.findall("(?::|;|=)(?:-)?(?:\)|\(|D|P)", text)
+    text = re.sub("[\W]+", " ", text.lower()) + " ".join(emoticons).replace("-", "")
     return text
 
-model = joblib.load('spam_classifier.joblib')
+
+model = joblib.load("spam_classifier.joblib")
 class_names = list(model.classes_)
 
 
@@ -31,25 +32,21 @@ class_names = list(model.classes_)
 def classify_message(model, message):
     label = model.predict([message])[0]
     probs = model.predict_proba([message])
-    return {
-        'label': label, 
-        'probability': probs[0][class_names.index(label)]
-    }
+    return {"label": label, "probability": probs[0][class_names.index(label)]}
 
-if message_text != '':
-  result = classify_message(model, message_text)
-  st.write(result)
 
-    
+if message_text != "":
+    result = classify_message(model, message_text)
+    st.write(result)
+
 
 # explaining predictions with lime
-explain_pred = st.button('Explain Predictions')
+explain_pred = st.button("Explain Predictions")
 
 if explain_pred:
-    with st.spinner('Generating explanations'):
+    with st.spinner("Generating explanations"):
         explainer = LimeTextExplainer(class_names=class_names)
         exp = explainer.explain_instance(
-            message_text, 
-            model.predict_proba,
-            num_features=10)
+            message_text, model.predict_proba, num_features=10
+        )
         components.html(exp.as_html(), height=800)
